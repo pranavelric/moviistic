914cc06638bbd235b0e3133991cae9fb21cf88ec
Pranav Choudhary <2018184@iiitdmj.ac.in>
2022-05-21 18:27:18 +0530

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.scss';
+import './assets/boxicons-2.1.2/css/boxicons.min.css';
+import 'swiper/swiper.min.css';
+
+
+
+
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import AppRoutes from './config/Routes';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-    <h1>My app</h1>  
-    </div>
+    <BrowserRouter>
+        <Header/>
+        <AppRoutes/>
+        <Footer/>
+    </BrowserRouter>
   );
 }
 
---
+++ src/config/Routes.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Route, Routes} from 'react-router-dom';
+
+import Catalog from '../pages/Catalog';
+import Detail from '../pages/Detail';
+import Home from '../pages/Home';
+
+
+
+const AppRoutes = ()=> {
+  return (
+    <Routes>
+      <Route path="/"
+      element={<Home/>}
+      />
+      <Route path='/:category/search/:keyword'
+       element={ <Catalog/>}
+      />
+      <Route path='/:category/id'
+      element={ <Detail/>}
+      />
+      <Route path='/:category'
+      element={ <Catalog/>}
+      />
+    </Routes>
+    );
+}
+
+export default AppRoutes;
