c934382d5ef0afecd07a0d9c0e7a255e79aebeed
Pranav Choudhary <2018184@iiitdmj.ac.in>
2022-07-31 19:00:56 +0530

    Home Component added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeroSlide from '../components/hero-slide/HeroSlide'
+
+export default function Home() {
+  return (
+    <div>
+            <HeroSlide/>
+    </div>
+  )
+}
