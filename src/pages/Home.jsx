199abfaac522e28d7d6b27132d0fc52f6f1c816a
Pranav Choudhary <2018184@iiitdmj.ac.in>
2022-08-01 00:03:41 +0530

    Movie List added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
-import HeroSlide from '../components/hero-slide/HeroSlide'
+import React from 'react';
+import HeroSlide from '../components/hero-slide/HeroSlide';
+import {Link} from 'react-router-dom';
+import { OutlineButton } from '../components/button/Button';
+
+import { CATEGORY, movieType, tvType } from '../api/tmdbApi';
+import MovieList from '../components/movie-list/MovieList';
+
 
 export default function Home() {
   return (
-    <div>
+    <>
             <HeroSlide/>
-    </div>
+            <div className="container">
+              <div className="section mb-3">
+                <div className="section_header mb-2">
+                  <h2>Trending Movies</h2>
+                  <Link to="/movie">
+                      <OutlineButton className="small">View more</OutlineButton>
+                  </Link>
+                </div>
+                  <MovieList category={CATEGORY.movie} type={movieType.popular}/>
+              </div>
+
+              <div className="section mb-3">
+                <div className="section_header mb-2">
+                  <h2>Top Rated Movies</h2>
+                  <Link to="/movie">
+                      <OutlineButton className="small">View more</OutlineButton>                    
+                  </Link>
+                </div>
+                <MovieList category={CATEGORY.movie} type={movieType.top_rated}/>
+              </div>
+
+              <div className="section mb-3">
+                    <div className="section_header mb-2">
+                        <h2>Trending TV</h2>
+                        <Link to="/tv">
+                            <OutlineButton className="small">View more</OutlineButton>
+                        </Link>
+                    </div>
+                    <MovieList category={CATEGORY.tv} type={tvType.popular}/>
+              </div>
+
+              <div className="section mb-3">
+                <div className="section_header mb-2">
+                  <h2>Top Rated TV</h2>
+                  <Link to="/tv">
+                      <OutlineButton className="small">View more</OutlineButton>
+                  </Link>
+                </div>
+                <MovieList category={CATEGORY.tv} type={tvType.top_rated}/>
+              </div>
+
+
+            </div>
+    </>
   )
 }
